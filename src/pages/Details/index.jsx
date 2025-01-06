7817841aba3c657901912c4f44f5a931df48d036
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-06 20:03:48 -0300

    add component Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container ,Links} from "./styles"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
+import {Tag} from "../../components/Tag"
 
 export function Details() {
   return (
@@ -19,6 +20,11 @@ export function Details() {
           </li>
           
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+      <Tag title = "express"/>
+      <Tag title = "node"/>
       </Section>
 
       <Button title='Voltar'/>
