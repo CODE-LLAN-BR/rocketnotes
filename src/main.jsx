3460cf9e5557916f140966db368490d0aa7490cf
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-06 10:45:38 -0300

    insert global style and custom fonts

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './pages/Styles/theme'
+import GlobalStyles from './pages/Styles/global'
+import { Details } from './pages/Details'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles"
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Leonardo Nunes</span>
+      <br />
+      <a>code-llan-br</a>
+    </Container>
+  )
+}
