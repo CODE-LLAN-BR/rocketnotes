20c8eb8796d50188862c3c5326624614fbdb82cf
Leonardo Nunes <leonardonunes1314@gmail.com>
2025-01-07 08:40:32 -0300

    insert home grid

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components'
 
 import theme from './pages/Styles/theme'
 import GlobalStyles from './pages/Styles/global'
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
